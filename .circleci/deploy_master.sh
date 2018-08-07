b8cf41988704033e284e9e6bb4b693e2b4342562
Damian <odissey@gmail.com>
2018-08-07 12:31:00 +0100

    Changed the debug apk name

--- .circleci/deploy_master.sh
+++ .circleci/deploy_master.sh
@@ -1,6 +1,6 @@
 curl \
 -F "status=2" \
 -F "notify=0" \
--F "ipa=StarWarsCharacterInfo/build/outputs/apk/app-debug.apk" \
+-F "ipa=app/build/outputs/apk/app-debug.apk" \
 -H "X-HockeyAppToken: 239a1780f9d44347928409370e36fe2c" \
 https://rink.hockeyapp.net/api/2/apps/StarwarsExample/app_versions/upload
